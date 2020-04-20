762e62e7857d6da44d19b5d8cacec19115e0aa9b
Dell Laptop <Dell Laptop@192.168.0.5>
2020-04-20 11:49:20 +0530

    first commit

---
+++ InventoryManagementSystem/src/main/java/com/capgemini/inventorymanagement/controllers/usercontroller.java
@@ -0,0 +1,5 @@
+package com.capgemini.inventorymanagement.controllers;
+
+public class usercontroller {
+
+}
---
+++ InventoryManagementSystem/src/main/java/com/capgemini/inventorymanagement/dao/userdao.java
@@ -0,0 +1,5 @@
+package com.capgemini.inventorymanagement.dao;
+
+public class userdao {
+
+}
