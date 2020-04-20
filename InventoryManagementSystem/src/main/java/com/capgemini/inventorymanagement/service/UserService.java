a791793c8112a376cde294926ac927bab6034a48
Dell Laptop <Dell Laptop@192.168.0.5>
2020-04-20 13:57:32 +0530

    second commit

---
+++ InventoryManagementSystem/src/main/java/com/capgemini/inventorymanagement/service/UserService.java
@@ -0,0 +1,16 @@
+package com.capgemini.inventorymanagement.service;
+
+import java.util.List;
+
+import com.capgemini.inventorymanagement.entities.Userdata;	
+
+public interface UserService {
+
+	Userdata addUser(Userdata u);
+	
+	List<Userdata>getAllUsers();
+
+	Userdata deleteUser(int userId);
+
+	Userdata updateUser(Userdata u);
+}
--- InventoryManagementSystem/src/main/java/com/capgemini/inventorymanagement/service/userservice.java
+++
@@ -1,5 +0,0 @@
-package com.capgemini.inventorymanagement.service;
-
-public class userservice {
-
-}
