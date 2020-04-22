cfdc48ad286ee54279588d0a9a2ba154e13a92d2
Sanda1922 <sandaniharika22@gmail.com>
2020-04-22 03:00:13 -0700

    niharika commit

--- InventoryManagementSystem/src/main/java/com/capgemini/inventorymanagement/entities/RawMaterialDetails.java
+++ InventoryManagementSystem/src/main/java/com/capgemini/inventorymanagement/entities/RawMaterialDetails.java
@@ -1,10 +1,10 @@
 package com.capgemini.inventorymanagement.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+//import javax.persistence.Entity;
+//import javax.persistence.Table;
 
-@Entity
-@Table(name = "RawMaterialStock")
+//@Entity
+//@Table(name = "RawMaterialStock")
 public class RawMaterialDetails {
 
 }
